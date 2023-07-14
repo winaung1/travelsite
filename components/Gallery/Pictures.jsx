1543a399e94a449339a56f5799690f549953c913
Win Aung <91712429+winaung1@users.noreply.github.com>
2023-07-13 18:25:47 -0600

    yes

--- components/Gallery/Pictures.jsx
+++ components/Gallery/Pictures.jsx
@@ -5,7 +5,7 @@ import {useState, useEffect} from 'react'
 export const Pictures = ({loading}) => {
     const [images, setImages] = useState([])
     const [original, setOriginal] = useState([])
-    const [activeState, setActiveState] = useState(false)
+    const [activeState, setActiveState] = useState(true)
     const [id, setId] = useState(1)
     const fetchPictures = async (userSearch) => {
         const getItem = localStorage.getItem(userSearch)
@@ -68,9 +68,9 @@ export const Pictures = ({loading}) => {
             }} className={id == 3 ? 'border-green-500 rounded border-2 px-4 py-1 text-white' : 'border-2 px-4 py-1 rounded hover:border-green-500 bg-white'}>Switzerland</button>
     </div>
     <div className='flex flex-wrap gap-4 justify-center items-center'>
-        {/* {activeState ? (original.map(img => <Image className='w-80 h-40 object-cover object-center' alt='' key={img.id} src={img?.src?.original} width={img.width} height={img.height}/>)) */}
-        {images.map(img => <Image className='w-80 h-40 object-cover object-center' alt='' key={img.id} src={img?.src?.original} width={img.width} height={img.height}/>)}
-        {/* } */}
+        {activeState ? <p className='text-white text-4xl font-bold h-80 pt-4'>There&apos;s no image yet. Please select which countries you want.</p>
+        : images.map(img => <Image className='w-80 h-40 object-cover object-center' alt='' key={img.id} src={img?.src?.original} width={img.width} height={img.height}/>)
+        }
     </div>
 
     </>
